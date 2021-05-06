59d5044bb68ddba15253cd793ef21385ea666730
tomanovy <tomanovy@gmail.com>
2021-05-06 23:02:45 +0200

    health-check simple controller finalization

--- dist/app.module.js
+++ dist/app.module.js
@@ -7,6 +7,8 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
+const terminus_1 = require("@nestjs/terminus");
+const health_controller_1 = require("./health/health.controller");
 const common_1 = require("@nestjs/common");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
@@ -14,8 +16,8 @@ let AppModule = class AppModule {
 };
 AppModule = __decorate([
     common_1.Module({
-        imports: [],
-        controllers: [app_controller_1.AppController,],
+        imports: [terminus_1.TerminusModule],
+        controllers: [app_controller_1.AppController, health_controller_1.HealthController],
         providers: [app_service_1.AppService],
     })
 ], AppModule);
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
+import { TerminusModule } from '@nestjs/terminus';
+import { HealthController } from './health/health.controller';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController, ],
+  imports: [TerminusModule],
+  controllers: [AppController, HealthController ],
   providers: [AppService],
 })
 export class AppModule {}
